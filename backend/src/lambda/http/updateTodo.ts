e69a51a8cc7c588fb0a2ae3bd83778c20e03dac7
KaiserPhemi <kaiser.phemi@protonmail.ch>
2021-11-03 21:15:25 +0100

    feature()
    - refactor handlers

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,26 +1,30 @@
+// third-party libraries
 import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-// import { updateTodo } from '../../businessLogic/todos'
+// helper functions
 import { updateTodo } from '../../helpers/todos';
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
-// import { getUserId } from '../utils'
 
+// utils
+import { getUserId } from '../utils'
+
+// handler
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // const todoId = event.pathParameters.todoId
+    const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body);
-    const authorization = event.headers.Authorization;
-    const split = authorization.split(' ');
-    const jwtToken = split[1];
+    const userId = getUserId(event)
+
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-    await updateTodo(updatedTodo, jwtToken);
+    await updateTodo(todoId, updatedTodo);
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
       },
       body: JSON.stringify({
         updatedTodo
